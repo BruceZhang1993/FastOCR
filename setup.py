431590bfc44ac870f2028b35aa8161fbed34dabe
Bruce Zhang <zttt183525594@gmail.com>
2021-02-16 22:38:52 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ readme = ''
 setup(
     long_description=readme,
     name='fastocr',
-    version='0.1.1',
+    version='0.1.2',
     description='FastOCR is a desktop application for OCR API.',
     python_requires='<3.10,>=3.7',
     project_urls={"documentation": "https://github.com/BruceZhang1993/FastOCR", "homepage": "https://github.com/BruceZhang1993/FastOCR", "repository": "https://github.com/BruceZhang1993/FastOCR"},
@@ -24,9 +24,9 @@ setup(
     license='LGPL-3.0-only',
     keywords='ocr',
     entry_points={"console_scripts": ["fastocr = fastocr.__main__:main"]},
-    packages=['fastocr'],
+    packages=['fastocr', 'fastocr.third_party'],
     package_dir={"": "."},
     package_data={"fastocr": ["qml/*.qml"]},
-    install_requires=['baidu-aip==2.*,>=2.2.18', 'dbus-python==1.*,>=1.2.16', 'pyside2==5.*,>=5.15.2', 'qasync==0.*,>=0.13.0'],
+    install_requires=['baidu-aip==2.*,>=2.2.18', 'click==7.*,>=7.1.2', 'dbus-python==1.*,>=1.2.16', 'pyside2==5.*,>=5.15.2', 'qasync==0.*,>=0.13.0'],
     extras_require={"dev": ["dephell==0.*,>=0.8.3"]},
 )
