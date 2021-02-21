fa3e6e618e2b33f43dd376648cc0196a8f6bf5cf
Bruce Zhang <zttt183525594@gmail.com>
2021-02-21 14:31:47 +0800

    add --version option

--- fastocr/__main__.py
+++ fastocr/__main__.py
@@ -3,15 +3,27 @@ import sys
 from pathlib import Path
 
 import click
+import pkg_resources
 from PySide2.QtWidgets import QApplication
 from qasync import QEventLoop
 
+from fastocr import __appname__
 from fastocr.i18n import Translation
 from fastocr.tray import AppTray
 from fastocr.util import instance_already_running, DesktopInfo
 
+__version__ = pkg_resources.get_distribution('fastocr').version
+
+
+def print_version(ctx, _, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(f'{__appname__} - Version {__version__}')
+    ctx.exit()
+
 
 @click.group(invoke_without_command=True)
+@click.option('--version', is_flag=True, callback=print_version, expose_value=False, is_eager=True)
 @click.pass_context
 def main(ctx):
     if ctx.invoked_subcommand is None:
