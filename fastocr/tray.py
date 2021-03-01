ca8198112bfa5ecdaac9213d6b157bfb29aaa5e4
Bruce Zhang <zttt183525594@gmail.com>
2021-03-01 14:15:51 +0800

    workaround for kde svg issue

--- fastocr/tray.py
+++ fastocr/tray.py
@@ -130,7 +130,10 @@ class AppTray(QSystemTrayIcon):
             path = Path(__file__).parent / 'resource' / 'icon' / 'dark'
         else:
             path = Path(__file__).parent / 'resource' / 'icon' / 'light'
-        self.setIcon(QIcon.fromTheme('fastocr-tray', QIcon((path / 'fastocr-tray.svg').as_posix())))
+        if DesktopInfo.desktop_environment() == DesktopInfo.KDE:
+            self.setIcon(QIcon.fromTheme('fastocr-tray', QIcon((path / 'fastocr-tray.png').as_posix())))
+        else:
+            self.setIcon(QIcon.fromTheme('fastocr-tray', QIcon((path / 'fastocr-tray.svg').as_posix())))
         self.activated.connect(self.activate_action)
         # Context menu
         self.setContextMenu(QMenu())
