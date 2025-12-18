111b1c06bd35d0d5ed24f3ca80a15c9c67a8dd96
Bruce Zhang <zttt183525594@gmail.com>
2025-12-18 16:09:30 +0800

    merge master

--- fastocr/data/i18n.pro
+++ fastocr/data/i18n.pro
@@ -1,4 +1,4 @@
 lupdate_only {
     SOURCES = ../qml/main.qml ../qml/component/*.qml
-    TRANSLATIONS = ../i18n/en.ts
+    TRANSLATIONS = ../i18n/en.ts ../i18n/zh_CN.ts
 }
--- fastocr/i18n/zh_CN.ts
+++ fastocr/i18n/zh_CN.ts
@@ -1,204 +1,204 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="zh-CN" sourcelanguage="en">
-  <context>
+<context>
     <name>ActionWizard</name>
     <message>
-      <location filename="../qml/component/ActionWizard.qml" line="48"/>
-      <source>Back</source>
-      <translation>上一步</translation>
+        <location filename="../qml/component/ActionWizard.qml" line="48"/>
+        <source>Back</source>
+        <translation>上一步</translation>
     </message>
     <message>
-      <location filename="../qml/component/ActionWizard.qml" line="55"/>
-      <location filename="../qml/component/ActionWizard.qml" line="73"/>
-      <source>Next</source>
-      <translation>下一步</translation>
+        <location filename="../qml/component/ActionWizard.qml" line="55"/>
+        <location filename="../qml/component/ActionWizard.qml" line="73"/>
+        <source>Next</source>
+        <translation>下一步</translation>
     </message>
     <message>
-      <location filename="../qml/component/ActionWizard.qml" line="68"/>
-      <source>Ok</source>
-      <translation>完成</translation>
+        <location filename="../qml/component/ActionWizard.qml" line="68"/>
+        <source>Ok</source>
+        <translation>完成</translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
     <name>Setting</name>
     <message>
-      <location filename="../qml/component/Setting.qml" line="29"/>
-      <source>General</source>
-      <translation type="unfinished">General</translation>
+        <location filename="../qml/component/Setting.qml" line="29"/>
+        <source>General</source>
+        <translation type="unfinished">General</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="41"/>
-      <source>Run at system start</source>
-      <translation type="unfinished">Run at system start</translation>
+        <location filename="../qml/component/Setting.qml" line="41"/>
+        <source>Run at system start</source>
+        <translation type="unfinished">Run at system start</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="51"/>
-      <source>Icon theme</source>
-      <translation>图标主题</translation>
+        <location filename="../qml/component/Setting.qml" line="51"/>
+        <source>Icon theme</source>
+        <translation>图标主题</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="62"/>
-      <source>Auto Select</source>
-      <translation>自动</translation>
+        <location filename="../qml/component/Setting.qml" line="62"/>
+        <source>Auto Select</source>
+        <translation>自动</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="63"/>
-      <source>Light</source>
-      <translation>亮色</translation>
+        <location filename="../qml/component/Setting.qml" line="63"/>
+        <source>Light</source>
+        <translation>亮色</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="64"/>
-      <source>Dark</source>
-      <translation>暗色</translation>
+        <location filename="../qml/component/Setting.qml" line="64"/>
+        <source>Dark</source>
+        <translation>暗色</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="70"/>
-      <source>Working mode</source>
-      <translation>工作模式</translation>
+        <location filename="../qml/component/Setting.qml" line="70"/>
+        <source>Working mode</source>
+        <translation>工作模式</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="81"/>
-      <source>Copy to clipboard automatic</source>
-      <translation type="unfinished">Copy to clipboard automatic</translation>
+        <location filename="../qml/component/Setting.qml" line="81"/>
+        <source>Copy to clipboard automatic</source>
+        <translation type="unfinished">Copy to clipboard automatic</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="82"/>
-      <source>Show notification to copy (Cannot click on notifications on some platforms)</source>
-      <translation type="unfinished">Show notification to copy (Cannot click on notifications on some platforms)</translation>
+        <location filename="../qml/component/Setting.qml" line="82"/>
+        <source>Show notification to copy (Cannot click on notifications on some platforms)</source>
+        <translation type="unfinished">Show notification to copy (Cannot click on notifications on some platforms)</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="89"/>
-      <source>Default text backend</source>
-      <translation type="unfinished">Default text backend</translation>
+        <location filename="../qml/component/Setting.qml" line="89"/>
+        <source>Default text backend</source>
+        <translation type="unfinished">Default text backend</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="104"/>
-      <location filename="../qml/component/Setting.qml" line="161"/>
-      <source>BaiduOCR</source>
-      <translation>百度文字识别服务</translation>
+        <location filename="../qml/component/Setting.qml" line="104"/>
+        <location filename="../qml/component/Setting.qml" line="161"/>
+        <source>BaiduOCR</source>
+        <translation>百度文字识别服务</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="113"/>
-      <location filename="../qml/component/Setting.qml" line="272"/>
-      <source>YoudaoOCR</source>
-      <translation>有道 OCR 识别服务</translation>
+        <location filename="../qml/component/Setting.qml" line="113"/>
+        <location filename="../qml/component/Setting.qml" line="272"/>
+        <source>YoudaoOCR</source>
+        <translation>有道 OCR 识别服务</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="122"/>
-      <location filename="../qml/component/Setting.qml" line="313"/>
-      <source>Face++OCR</source>
-      <translation>Face++ OCR 识别服务</translation>
+        <location filename="../qml/component/Setting.qml" line="122"/>
+        <location filename="../qml/component/Setting.qml" line="313"/>
+        <source>Face++OCR</source>
+        <translation>Face++ OCR 识别服务</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="131"/>
-      <source>PaddleOCR</source>
-      <translation type="unfinished">PaddleOCR</translation>
+        <location filename="../qml/component/Setting.qml" line="131"/>
+        <source>PaddleOCR</source>
+        <translation type="unfinished">PaddleOCR</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="137"/>
-      <source>Default formula backend</source>
-      <translation type="unfinished">Default formula backend</translation>
+        <location filename="../qml/component/Setting.qml" line="137"/>
+        <source>Default formula backend</source>
+        <translation type="unfinished">Default formula backend</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="152"/>
-      <source>Mathpix</source>
-      <translation type="unfinished">Mathpix</translation>
+        <location filename="../qml/component/Setting.qml" line="152"/>
+        <source>Mathpix</source>
+        <translation type="unfinished">Mathpix</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="187"/>
-      <location filename="../qml/component/Setting.qml" line="285"/>
-      <source>APP_ID</source>
-      <translation type="unfinished">APP_ID</translation>
+        <location filename="../qml/component/Setting.qml" line="187"/>
+        <location filename="../qml/component/Setting.qml" line="285"/>
+        <source>APP_ID</source>
+        <translation type="unfinished">APP_ID</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="199"/>
-      <location filename="../qml/component/Setting.qml" line="326"/>
-      <source>API_KEY</source>
-      <translation type="unfinished">API_KEY</translation>
+        <location filename="../qml/component/Setting.qml" line="199"/>
+        <location filename="../qml/component/Setting.qml" line="326"/>
+        <source>API_KEY</source>
+        <translation type="unfinished">API_KEY</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="211"/>
-      <source>SECRET_KEY</source>
-      <translation type="unfinished">SECRET_KEY</translation>
+        <location filename="../qml/component/Setting.qml" line="211"/>
+        <source>SECRET_KEY</source>
+        <translation type="unfinished">SECRET_KEY</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="223"/>
-      <source>Languages</source>
-      <translation>语言</translation>
+        <location filename="../qml/component/Setting.qml" line="223"/>
+        <source>Languages</source>
+        <translation>语言</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="246"/>
-      <source>Default language</source>
-      <translation type="unfinished">Default language</translation>
+        <location filename="../qml/component/Setting.qml" line="246"/>
+        <source>Default language</source>
+        <translation type="unfinished">Default language</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="259"/>
-      <source>Use accurate mode</source>
-      <translation>使用高精度模式</translation>
+        <location filename="../qml/component/Setting.qml" line="259"/>
+        <source>Use accurate mode</source>
+        <translation>使用高精度模式</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="297"/>
-      <source>SEC_KEY</source>
-      <translation type="unfinished">SEC_KEY</translation>
+        <location filename="../qml/component/Setting.qml" line="297"/>
+        <source>SEC_KEY</source>
+        <translation type="unfinished">SEC_KEY</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="338"/>
-      <source>API_SEC</source>
-      <translation type="unfinished">API_SEC</translation>
+        <location filename="../qml/component/Setting.qml" line="338"/>
+        <source>API_SEC</source>
+        <translation type="unfinished">API_SEC</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="354"/>
-      <source>Mathpix OCR</source>
-      <translation type="unfinished">Mathpix OCR</translation>
+        <location filename="../qml/component/Setting.qml" line="354"/>
+        <source>Mathpix OCR</source>
+        <translation type="unfinished">Mathpix OCR</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="367"/>
-      <source>App ID</source>
-      <translation type="unfinished">App ID</translation>
+        <location filename="../qml/component/Setting.qml" line="367"/>
+        <source>App ID</source>
+        <translation type="unfinished">App ID</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="379"/>
-      <source>App Key</source>
-      <translation type="unfinished">App Key</translation>
+        <location filename="../qml/component/Setting.qml" line="379"/>
+        <source>App Key</source>
+        <translation type="unfinished">App Key</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="445"/>
-      <source>Save</source>
-      <translation>保存</translation>
+        <location filename="../qml/component/Setting.qml" line="445"/>
+        <source>Save</source>
+        <translation>保存</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="455"/>
-      <source>Apply</source>
-      <translation>应用</translation>
+        <location filename="../qml/component/Setting.qml" line="455"/>
+        <source>Apply</source>
+        <translation>应用</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="463"/>
-      <source>Close</source>
-      <translation>关闭</translation>
+        <location filename="../qml/component/Setting.qml" line="463"/>
+        <source>Close</source>
+        <translation>关闭</translation>
     </message>
     <message>
-      <location filename="../qml/component/Setting.qml" line="468"/>
-      <source>Open in editor</source>
-      <translation>手动编辑</translation>
+        <location filename="../qml/component/Setting.qml" line="468"/>
+        <source>Open in editor</source>
+        <translation>手动编辑</translation>
     </message>
-  </context>
-  <context>
+</context>
+<context>
     <name>main</name>
     <message>
-      <location filename="../qml/main.qml" line="12"/>
-      <source>FastOCR Setting</source>
-      <translation>FastOCR 设置</translation>
+        <location filename="../qml/main.qml" line="12"/>
+        <source>FastOCR Setting</source>
+        <translation>FastOCR 设置</translation>
     </message>
     <message>
-      <location filename="../qml/main.qml" line="33"/>
-      <source>Setting</source>
-      <translation>设置</translation>
+        <location filename="../qml/main.qml" line="33"/>
+        <source>Setting</source>
+        <translation>设置</translation>
     </message>
     <message>
-      <location filename="../qml/main.qml" line="39"/>
-      <source>Actions</source>
-      <translation>动作</translation>
+        <location filename="../qml/main.qml" line="39"/>
+        <source>Actions</source>
+        <translation>动作</translation>
     </message>
-  </context>
+</context>
 </TS>
